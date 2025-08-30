6b690c4e9ecb2996973b2aabb085c82ec87f8edf
left <107749872+0xleft@users.noreply.github.com>
2025-08-30 23:11:09 +0200

    more testing stuff

--- asm/test.sh
+++ asm/test.sh
@@ -1,5 +1,5 @@
-stack run > temp.asm
-nasm temp.asm -f elf64
-ld -m elf_x86_64 -s -o temp temp.o
-rm temp.o
-./temp
+bfcompiler ../test/test22.bf 100000
+nasm test22.asm -f elf64
+ld -m elf_x86_64 -s -o test22 test22.o
+rm test22.o
+./test22
